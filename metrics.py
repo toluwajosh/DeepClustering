3a06f0f262adb631ac859569437d9c59410e9809
Joshua Owoyemi <joshua.owoyemi@elix-inc.com>
2020-06-07 03:43:26 +0900

    Update: applied black, replaced deprecated function

--- metrics.py
+++ metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.metrics import normalized_mutual_info_score, adjusted_rand_score
+from scipy.optimize import linear_sum_assignment
 
 nmi = normalized_mutual_info_score
 ari = adjusted_rand_score
@@ -13,6 +14,12 @@ def acc(y_true, y_pred):
         y_pred: predicted labels, numpy.array with shape `(n_samples,)`
     # Return
         accuracy, in [0,1]
+    
+    Note: the original code used:
+    `from sklearn.utils.linear_assignment_ import linear_assignment`
+    but this was no more available in most recent version of sklearn so it was
+    substituted with
+    from scipy.optimize import linear_sum_assignment
     """
     y_true = y_true.astype(np.int64)
     assert y_pred.size == y_true.size
@@ -20,6 +27,8 @@ def acc(y_true, y_pred):
     w = np.zeros((D, D), dtype=np.int64)
     for i in range(y_pred.size):
         w[y_pred[i], y_true[i]] += 1
-    from sklearn.utils.linear_assignment_ import linear_assignment
-    ind = linear_assignment(w.max() - w)
-    return sum([w[i, j] for i, j in ind]) * 1.0 / y_pred.size
+
+    # TODO: verify that this function is same as
+    # linear_sum_assignment in scipy.optimize
+    row_ind, col_ind = linear_sum_assignment(w.max() - w)
+    return sum([w[i, j] for i, j in zip(row_ind, col_ind)]) * 1.0 / y_pred.size
